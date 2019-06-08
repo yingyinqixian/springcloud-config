7228e49852530bc22f51e789afe4f325376f2856
yingyinqi <710441971@qqcom>
2019-06-08 18:27:59 +0800

    springcloud分布式配置中心

---
+++ springboot2.0-config_client/src/main/java/com/itmayiedu/api/controller/TestController.java
@@ -0,0 +1,34 @@
+/**
+ * 功能说明:
+ * 功能作者:
+ * 创建日期:
+ * 版权归属:每特教育|蚂蚁课堂所有 www.itmayiedu.com
+ */
+package com.itmayiedu.api.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 功能说明: <br>
+ * 创建作者:每特教育-余胜军<br>
+ * 创建时间:2018年9月10日 下午8:51:46<br>
+ * 教育机构:每特教育|蚂蚁课堂<br>
+ * 版权说明:上海每特教育科技有限公司版权所有<br>
+ * 官方网站:www.itmayiedu.com|www.meitedu.com<br>
+ * 联系方式:qq644064779<br>
+ * 注意:本内容有每特教育学员共同研发,请尊重原创版权
+ */
+@RestController
+public class TestController {
+
+	@Value("${userAge}")
+	private String userAge;
+
+	@RequestMapping("/userAge")
+	public String getUserName() {
+		return userAge;
+	}
+
+}
---
+++ springcloud-2.0-eureka-server/src/main/java/com/itmayiedu/AppEureka8100.java
@@ -0,0 +1,31 @@
+/**
+ * 功能说明:
+ * 功能作者:
+ * 创建日期:
+ * 版权归属:每特教育|蚂蚁课堂所有 www.itmayiedu.com
+ */
+package com.itmayiedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 功能说明: <br>
+ * 创建作者:每特教育-余胜军<br>
+ * 创建时间:2018年9月3日 下午9:27:08<br>
+ * 教育机构:每特教育|蚂蚁课堂<br>
+ * 版权说明:上海每特教育科技有限公司版权所有<br>
+ * 官方网站:www.itmayiedu.com|www.meitedu.com<br>
+ * 联系方式:qq644064779<br>
+ * 注意:本内容有每特教育学员共同研发,请尊重原创版权
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class AppEureka8100 {
+
+	// @EnableEurekaServer 表示开启EurekaServer服务 开启注册中心
+	public static void main(String[] args) {
+		SpringApplication.run(AppEureka8100.class, args);
+	}
+}
